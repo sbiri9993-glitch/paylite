df174b4c34d3584d95d1b8d1bb8c22886b525d57
Sadatou Biri <sbiri9993@gmail.com>
2026-04-23 11:06:37 +0100

    update gitugnore

---
+++ config/database.php
@@ -0,0 +1,30 @@
+<?php
+
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+define('MONGO_URI', 'mongodb+srv://sbiri9993_db_user:H7Zd7Xq6G46yxcTu@cluster0.qdne7qv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+define('MONGO_DB',  'paylite');
+
+
+try {
+    $mongoClient = new MongoDB\Client(MONGO_URI, [], [
+        'typeMap' => [
+            'array'    => 'array',
+            'document' => 'array',
+            'root'     => 'array',
+        ],
+    ]);
+
+    $mongoClient->selectDatabase(MONGO_DB)->command(['ping' => 1]);
+
+    $db = $mongoClient->selectDatabase(MONGO_DB);
+
+    $users        = $db->selectCollection('users');
+    $transactions = $db->selectCollection('transactions');
+
+} catch (MongoDB\Driver\Exception\Exception $e) {
+    http_response_code(500);
+    error_log('MongoDB connection error: ' . $e->getMessage());
+    die(json_encode(['success' => false, 'message' => 'Database connection failed.']));
+}
